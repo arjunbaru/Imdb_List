e65a988af50bb4f6c606f8130d62a13f872e6cec
Arjun Baru <arjun.baru2@gmail.com>
2022-06-25 15:57:45 +0530

    Initial Commit

---
+++ PhonePeDemo/PhonePeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhonePeDemoApp.swift
+//  PhonePeDemo
+//
+//  Created by Arjun Baru on 25/06/22.
+//
+
+import SwiftUI
+
+@main
+struct PhonePeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
